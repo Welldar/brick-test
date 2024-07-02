a8c01192f1927298c4048dabceba47099b7c2735
Zheryakov Eldar <eldarzheryakov@gmail.com>
2024-07-02 17:09:20 +0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+import { fontFamily } from 'tailwindcss/defaultTheme'
+import formsPlugin from '@tailwindcss/forms'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Roboto"', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [formsPlugin],
+}
