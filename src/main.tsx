ae19cb6d8053d9c849cb0b39b2076eebb4466a3c
Zheryakov Eldar <eldarzheryakov@gmail.com>
2024-07-03 13:10:55 +0500

    feat: define routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Layout } from './routes/layout'
+import { Root, RootLoader } from './routes/root'
+import { Character, CharacterLoader } from './routes/character'
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Root />,
+        loader: RootLoader,
+      },
+      {
+        path: '/character/:id',
+        element: <Character />,
+        loader: CharacterLoader,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
